d712206f752f8334efc5d91b1163e0ed80c24bae
Sathwik <mail.sathwik@gmail.com>
2026-07-24 12:51:26 +0530

    Add PostHog analytics

---
+++ docs/posthog.js
@@ -0,0 +1,9 @@
+!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init capture register register_once unregister identify reset opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+
+posthog.init("phc_uUPrNVS5h38DgdNbNzP3ae2SyfwByrLEoJ3CHP82JWwH", {
+  api_host: "https://us.i.posthog.com",
+  defaults: "2026-05-30",
+  person_profiles: "identified_only",
+  disable_session_recording: true,
+});
+posthog.register({ site: "commentrixai.com" });
